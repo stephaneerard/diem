6491dd663146557ca80c6a457027cad690e06ea7
Stéphane Erard <stephane.erard@gmail.com>
2010-11-28 00:19:36 +0100

    Removing nestedset widget and validator

--- dmCorePlugin/data/generator/dmDoctrineForm/default/template/sfDoctrineFormGeneratedTemplate.php
+++ dmCorePlugin/data/generator/dmDoctrineForm/default/template/sfDoctrineFormGeneratedTemplate.php
@@ -18,15 +18,10 @@ abstract class Base<?php echo $this->modelName ?>Form extends <?php echo $this->
 <?php foreach ($this->getColumns() as $column): ?>
       '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new <?php echo $this->getWidgetClassForColumn($column) ?>(<?php echo $this->getWidgetOptionsForColumn($column) ?>),
 <?php endforeach; ?>
-
 <?php foreach ($this->getManyToManyRelations() as $relation): ?>
-  <?php if ('DmMedia' === $relation->getClass()) continue; ?>
+<?php if ('DmMedia' === $relation->getClass()) continue; ?>
       '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($this->underscore($relation['alias']).'_list')) ?> => new sfWidgetFormDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'expanded' => true)),
 <?php endforeach; ?>
-
-<?php if($this->table->isNestedSet()):?>
-      'dm_ns_parent_id' => new sfWidgetFormDoctrineChoiceNestedSet(array('model' => '<?php echo $this->modelName?>', 'add_empty' => true)),
-<?php endif;?>
     ));
 
     $this->setValidators(array(
@@ -34,7 +29,7 @@ abstract class Base<?php echo $this->modelName ?>Form extends <?php echo $this->
       '<?php echo $column->getFieldName() ?>'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($column->getFieldName())) ?> => new <?php echo $this->getValidatorClassForColumn($column) ?>(<?php echo $this->getValidatorOptionsForColumn($column) ?>),
 <?php endforeach; ?>
 <?php foreach ($this->getManyToManyRelations() as $relation): ?>
-  <?php if ('DmMedia' === $relation->getClass()) continue; ?>
+<?php if ('DmMedia' === $relation->getClass()) continue; ?>
       '<?php echo $this->underscore($relation['alias']) ?>_list'<?php echo str_repeat(' ', $this->getColumnNameMaxLength() - strlen($this->underscore($relation['alias']).'_list')) ?> => new sfValidatorDoctrineChoice(array('multiple' => true, 'model' => '<?php echo $relation['table']->getOption('name') ?>', 'required' => false)),
 <?php endforeach; ?>
     ));
