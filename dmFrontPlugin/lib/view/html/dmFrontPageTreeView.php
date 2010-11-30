1ff5cf372375ec2f3fe19f525e6444119b87c7c8
Stéphane Erard <stephane.erard@gmail.com>
2010-11-30 16:53:54 +0100

    Fixing problem for URLs with i18n prefix, occuring for homepage link

--- dmFrontPlugin/lib/view/html/dmFrontPageTreeView.php
+++ dmFrontPlugin/lib/view/html/dmFrontPageTreeView.php
@@ -6,7 +6,7 @@ class dmFrontPageTreeView extends dmPageTreeView
   protected function renderPageLink(array $page)
   {
     if(sfConfig::get('dm_i18n_prefix_url')){
-      $page[6] = $this->culture . '/' . $page[6];
+      $page[6] = $this->culture . (strlen($page[6])>0 ? '/' . $page[6] : '');
     }
      
     return '<a href="'.$page[6].'" data-page-id="'.$page[0].'"><ins></ins>'.$page[5].'</a>';
